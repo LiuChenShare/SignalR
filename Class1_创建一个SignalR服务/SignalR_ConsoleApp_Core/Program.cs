802b6febac8d7829373347a4e5f462c0527b59f1
村长 <757891685@qq.com>
2019-07-10 16:51:42 +0800

    加入自动发消息的功能

---
+++ Class1_创建一个SignalR服务/SignalR_ConsoleApp_Core/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalR_ConsoleApp_Core
+{
+    class Program
+    {
+        HubConnection connection;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+
+        private void XXX()
+        {
+            connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:53353/ChatHub")
+                .Build();
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+        }
+
+        
+        private async void connectButton()
+        {
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                Console.WriteLine($"{user}: {message}");
+            });
+
+            await connection.StartAsync();
+        }
+
+        private async void sendButton(string user, string message)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendMessage", user, message);
+            }
+            catch (Exception ex)
+            {
+                //ConsoleHelper.WriteErrorLine($"{user}: {message}");
+
+                ConsoleHelper.WriteErrorLine(ex.ToString());
+            }
+        }
+    }
+    
+}
