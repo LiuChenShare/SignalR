3ab41da073dedf2ca6c9b54cdfc88c0fde5d7e58
村长 <757891685@qq.com>
2019-07-23 16:48:44 +0800

    若是读取不到配置文件，则使用默认的777和778端口

--- Sample1_聊天大厅及聊天室/SignalRHost/Program.cs
+++ Sample1_聊天大厅及聊天室/SignalRHost/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -20,11 +21,18 @@ namespace SignalRHost
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    var configuration = new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory)
-                                             .AddJsonFile("SignalRHost.json")
-                                             .Build();
-                    //webBuilder.UseUrls("http://*:2233", "http://*:4455");
-                    webBuilder.UseConfiguration(configuration);
+                    if (File.Exists("SignalRHost.json"))
+                    {
+                        var configuration = new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory)
+                                                 .AddJsonFile("SignalRHost.json")
+                                                 .Build();
+                        //webBuilder.UseUrls("http://*:2233", "http://*:4455");
+                        webBuilder.UseConfiguration(configuration);
+                    }
+                    else
+                    {
+                        webBuilder.UseUrls("http://*:777", "http://*:778");
+                    }
                     webBuilder.UseStartup<Startup>();
                 });
     }
