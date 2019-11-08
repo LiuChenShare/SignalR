494d0ff1bf0ba218572190c1b3e614bd0462efb4
村长 <757891685@qq.com>
2019-11-08 16:04:22 +0800

    添加SignalR支持及默认使用页面

--- Sample1_聊天大厅及聊天室升级版/SignalRHost/Startup.cs
+++ Sample1_聊天大厅及聊天室升级版/SignalRHost/Startup.cs
@@ -24,6 +24,8 @@ namespace SignalRHost
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();//Ìí¼ÓRazor
+            services.AddSignalR();//Ìí¼ÓSignalR
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +53,8 @@ namespace SignalRHost
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+                endpoints.MapHub<Hubs.ChatHub>("/chatHub");
             });
         }
     }
