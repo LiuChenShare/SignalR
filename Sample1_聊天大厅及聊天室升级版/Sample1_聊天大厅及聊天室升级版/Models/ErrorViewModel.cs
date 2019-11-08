0940f544365fa4fb82b38885804bf98cfe3c0bba
村长 <757891685@qq.com>
2019-11-08 15:09:20 +0800

    创建新项目

---
+++ Sample1_聊天大厅及聊天室升级版/Sample1_聊天大厅及聊天室升级版/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sample1_聊天大厅及聊天室升级版.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
