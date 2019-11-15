f8b0876acc04a714ded27d811dafa240175248ff
村长 <757891685@qq.com>
2019-11-15 16:28:22 +0800

    加入登录功能

--- Sample1_聊天大厅及聊天室升级版/SignalRHost/Controllers/HomeController.cs
+++ Sample1_聊天大厅及聊天室升级版/SignalRHost/Controllers/HomeController.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
+using ChatRoom.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SignalRHost.Datas;
 using SignalRHost.Models;
 
 namespace SignalRHost.Controllers
@@ -32,6 +35,29 @@ namespace SignalRHost.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Login(string account, string password)
+        {
+            dynamic data = new System.Dynamic.ExpandoObject(); //动态类型字段 可读可写
+            var aaaa = CentralizedData.Instance.UserList.Where(x => x.Account == account && x.Password == password).FirstOrDefault();
+            if(aaaa!= null)
+            {
+                data.Id = aaaa.Id;
+                data.Name = aaaa.Name;
+                ClientInfoModel model = new ClientInfoModel();
+                model.Id = aaaa.Id;
+                model.Name = aaaa.Name;
+                //return Json(new { Success = true, Messages = "登录成功", Data = JsonSerializer.Serialize(model) });
+                return Json(new { Success = true, Messages = "登录成功", Data = model });
+            }
+            return Json(new { Success = false, Messages = "登录失败" });
         }
     }
 }
--- Sample1_聊天大厅及聊天室升级版/SignalRHost/Hubs/ChatHub.cs
+++ Sample1_聊天大厅及聊天室升级版/SignalRHost/Hubs/ChatHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalRHost.Datas;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,29 @@ namespace SignalRHost.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendAllMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveAllMessage", user, message);
+        }
+
+        /// <summary>
+        /// 服务注册
+        /// </summary>
+        /// <param name="clientId"></param>
+        /// <returns></returns>
+        public async Task Register(Guid clientId)
+        {
+            //if (CentralizedData.Instance.ClientAddress.ContainsKey(clientId))
+            //{
+            //    ClientData.Instance.ClientAddress[clientId] = Context.ConnectionId;
+            //}
+            //else
+            //{
+            //    ClientData.Instance.ClientAddress.Add(clientId, Context.ConnectionId);
+            //}
+            //await Clients.Caller.SendAsync("RegisterNotice", ClientData.Instance.ClientAddress.Count());
+            //await Clients.All.SendAsync("ServerNotice", "欢迎" + clientId.ToString() + "加入房间");
+            //ConsoleHelper.WriteSuccessLine(clientId.ToString() + "加入房间");
         }
     }
 }
